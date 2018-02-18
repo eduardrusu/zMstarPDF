7b3cbbe913b2d106232d0fcaf4559029f35f2b21
edi <cerusu@Cristians-MacBook-Pro.local>
2018-02-17 14:09:32 -1000

    edits

---
+++ python/scripts/DESKTOP/batch6_insertstars.sh
@@ -0,0 +1,17 @@
+python /Users/cerusu/GITHUB/zMstarPDF/python/catalogue_utilities/kappamed_insertstarsnobeta.py WFI2033 GGL_los_8_5_0_N_4096_ang_4_rays_to_plane_35_f 23 45 measured 5 0.61 0.71
+python /Users/cerusu/GITHUB/zMstarPDF/python/catalogue_utilities/kappamed_insertstarsnobeta.py WFI2033 GGL_los_8_5_1_N_4096_ang_4_rays_to_plane_35_f 23 45 measured 5 0.61 0.71
+python /Users/cerusu/GITHUB/zMstarPDF/python/catalogue_utilities/kappamed_insertstarsnobeta.py WFI2033 GGL_los_8_5_2_N_4096_ang_4_rays_to_plane_35_f 23 45 measured 5 0.61 0.71
+python /Users/cerusu/GITHUB/zMstarPDF/python/catalogue_utilities/kappamed_insertstarsnobeta.py WFI2033 GGL_los_8_5_3_N_4096_ang_4_rays_to_plane_35_f 23 45 measured 5 0.61 0.71
+python /Users/cerusu/GITHUB/zMstarPDF/python/catalogue_utilities/kappamed_insertstarsnobeta.py WFI2033 GGL_los_8_5_4_N_4096_ang_4_rays_to_plane_35_f 23 45 measured 5 0.61 0.71
+python /Users/cerusu/GITHUB/zMstarPDF/python/catalogue_utilities/kappamed_insertstarsnobeta.py WFI2033 GGL_los_8_5_5_N_4096_ang_4_rays_to_plane_35_f 23 45 measured 5 0.61 0.71
+python /Users/cerusu/GITHUB/zMstarPDF/python/catalogue_utilities/kappamed_insertstarsnobeta.py WFI2033 GGL_los_8_5_6_N_4096_ang_4_rays_to_plane_35_f 23 45 measured 5 0.61 0.71
+python /Users/cerusu/GITHUB/zMstarPDF/python/catalogue_utilities/kappamed_insertstarsnobeta.py WFI2033 GGL_los_8_5_7_N_4096_ang_4_rays_to_plane_35_f 23 45 measured 5 0.61 0.71
+
+python /Users/cerusu/GITHUB/zMstarPDF/python/catalogue_utilities/kappamed_insertstarsnobeta.py WFI2033 GGL_los_8_5_0_N_4096_ang_4_rays_to_plane_35_f 23 120 measured 5 0.61 0.71
+python /Users/cerusu/GITHUB/zMstarPDF/python/catalogue_utilities/kappamed_insertstarsnobeta.py WFI2033 GGL_los_8_5_1_N_4096_ang_4_rays_to_plane_35_f 23 120 measured 5 0.61 0.71
+python /Users/cerusu/GITHUB/zMstarPDF/python/catalogue_utilities/kappamed_insertstarsnobeta.py WFI2033 GGL_los_8_5_2_N_4096_ang_4_rays_to_plane_35_f 23 120 measured 5 0.61 0.71
+python /Users/cerusu/GITHUB/zMstarPDF/python/catalogue_utilities/kappamed_insertstarsnobeta.py WFI2033 GGL_los_8_5_3_N_4096_ang_4_rays_to_plane_35_f 23 120 measured 5 0.61 0.71
+python /Users/cerusu/GITHUB/zMstarPDF/python/catalogue_utilities/kappamed_insertstarsnobeta.py WFI2033 GGL_los_8_5_4_N_4096_ang_4_rays_to_plane_35_f 23 120 measured 5 0.61 0.71
+python /Users/cerusu/GITHUB/zMstarPDF/python/catalogue_utilities/kappamed_insertstarsnobeta.py WFI2033 GGL_los_8_5_5_N_4096_ang_4_rays_to_plane_35_f 23 120 measured 5 0.61 0.71
+python /Users/cerusu/GITHUB/zMstarPDF/python/catalogue_utilities/kappamed_insertstarsnobeta.py WFI2033 GGL_los_8_5_6_N_4096_ang_4_rays_to_plane_35_f 23 120 measured 5 0.61 0.71
+python /Users/cerusu/GITHUB/zMstarPDF/python/catalogue_utilities/kappamed_insertstarsnobeta.py WFI2033 GGL_los_8_5_7_N_4096_ang_4_rays_to_plane_35_f 23 120 measured 5 0.61 0.71
---
+++ python/scripts/DESKTOP/batch7_insertstars.sh
@@ -0,0 +1,17 @@
+python /Users/cerusu/GITHUB/zMstarPDF/python/catalogue_utilities/kappamed_insertstarsnobeta.py WFI2033 GGL_los_8_6_0_N_4096_ang_4_rays_to_plane_35_f 23 45 measured 5 0.61 0.71
+python /Users/cerusu/GITHUB/zMstarPDF/python/catalogue_utilities/kappamed_insertstarsnobeta.py WFI2033 GGL_los_8_6_1_N_4096_ang_4_rays_to_plane_35_f 23 45 measured 5 0.61 0.71
+python /Users/cerusu/GITHUB/zMstarPDF/python/catalogue_utilities/kappamed_insertstarsnobeta.py WFI2033 GGL_los_8_6_2_N_4096_ang_4_rays_to_plane_35_f 23 45 measured 5 0.61 0.71
+python /Users/cerusu/GITHUB/zMstarPDF/python/catalogue_utilities/kappamed_insertstarsnobeta.py WFI2033 GGL_los_8_6_3_N_4096_ang_4_rays_to_plane_35_f 23 45 measured 5 0.61 0.71
+python /Users/cerusu/GITHUB/zMstarPDF/python/catalogue_utilities/kappamed_insertstarsnobeta.py WFI2033 GGL_los_8_6_4_N_4096_ang_4_rays_to_plane_35_f 23 45 measured 5 0.61 0.71
+python /Users/cerusu/GITHUB/zMstarPDF/python/catalogue_utilities/kappamed_insertstarsnobeta.py WFI2033 GGL_los_8_6_5_N_4096_ang_4_rays_to_plane_35_f 23 45 measured 5 0.61 0.71
+python /Users/cerusu/GITHUB/zMstarPDF/python/catalogue_utilities/kappamed_insertstarsnobeta.py WFI2033 GGL_los_8_6_6_N_4096_ang_4_rays_to_plane_35_f 23 45 measured 5 0.61 0.71
+python /Users/cerusu/GITHUB/zMstarPDF/python/catalogue_utilities/kappamed_insertstarsnobeta.py WFI2033 GGL_los_8_6_7_N_4096_ang_4_rays_to_plane_35_f 23 45 measured 5 0.61 0.71
+
+python /Users/cerusu/GITHUB/zMstarPDF/python/catalogue_utilities/kappamed_insertstarsnobeta.py WFI2033 GGL_los_8_6_0_N_4096_ang_4_rays_to_plane_35_f 23 120 measured 5 0.61 0.71
+python /Users/cerusu/GITHUB/zMstarPDF/python/catalogue_utilities/kappamed_insertstarsnobeta.py WFI2033 GGL_los_8_6_1_N_4096_ang_4_rays_to_plane_35_f 23 120 measured 5 0.61 0.71
+python /Users/cerusu/GITHUB/zMstarPDF/python/catalogue_utilities/kappamed_insertstarsnobeta.py WFI2033 GGL_los_8_6_2_N_4096_ang_4_rays_to_plane_35_f 23 120 measured 5 0.61 0.71
+python /Users/cerusu/GITHUB/zMstarPDF/python/catalogue_utilities/kappamed_insertstarsnobeta.py WFI2033 GGL_los_8_6_3_N_4096_ang_4_rays_to_plane_35_f 23 120 measured 5 0.61 0.71
+python /Users/cerusu/GITHUB/zMstarPDF/python/catalogue_utilities/kappamed_insertstarsnobeta.py WFI2033 GGL_los_8_6_4_N_4096_ang_4_rays_to_plane_35_f 23 120 measured 5 0.61 0.71
+python /Users/cerusu/GITHUB/zMstarPDF/python/catalogue_utilities/kappamed_insertstarsnobeta.py WFI2033 GGL_los_8_6_5_N_4096_ang_4_rays_to_plane_35_f 23 120 measured 5 0.61 0.71
+python /Users/cerusu/GITHUB/zMstarPDF/python/catalogue_utilities/kappamed_insertstarsnobeta.py WFI2033 GGL_los_8_6_6_N_4096_ang_4_rays_to_plane_35_f 23 120 measured 5 0.61 0.71
+python /Users/cerusu/GITHUB/zMstarPDF/python/catalogue_utilities/kappamed_insertstarsnobeta.py WFI2033 GGL_los_8_6_7_N_4096_ang_4_rays_to_plane_35_f 23 120 measured 5 0.61 0.71
