0c9fb5661f3bd64a6c45d2d045276aba9d4cc3c5
Cristian Eduard Rusu <cerusu@ucdavis.edu>
2018-09-19 15:44:34 -1000

    edit

--- python/image_utilities/HSCmatchcutoutpsf.py
+++ python/image_utilities/HSCmatchcutoutpsf.py
@@ -3,13 +3,13 @@
 
 #from astropy.io import fits
 import numpy as np
-#import os
-#import sys
+import os
 import glob
 from astropy.wcs import WCS
 from astropy.io import fits
 
 path = "/Volumes/LaCieSubaru/Gaia/James/"
+os.chdir(path)
 folder_cutout = glob.glob('arch*')
 folder_psf = glob.glob('psf-*')
 
@@ -40,23 +40,31 @@ for i in range(len(list_psf)):
 
 list_cutout_filter = np.empty(len(list_cutout),dtype="string")
 list_cutout_ra = np.zeros(len(list_cutout))
-list_cutout_dec = np.zeros(len(list_cutout))
+list_cutout_dec = np.zeros(len(list_cutout)) 
 for i in range(len(list_cutout)):
-    if i % 100 == 0: print i
+    if i % 1000 == 0: print i
     file = fits.open(list_cutout[i])
     w = WCS(file[1].header)
     coord = w.wcs_pix2world(21,21,1)
-    list_psf_ra[i] = coord[0]
-    list_psf_dec[i] = coord[1]
+    list_cutout_ra[i] = coord[0]
+    list_cutout_dec[i] = coord[1]
     file.close()
-    str = list_psf[i].split("-")
+    str = list_cutout[i].split("-")
     filter = str[7]
     list_cutout_filter[i] = filter
 
-np.savetxt("cutouts.cat",np.c_[list_cutout,list_cutout_filter,list_cutout_ra,list_cutout_dec])
-np.savetxt("psf.cat",np.c_[list_psf,list_psf_filter,list_psf_ra,list_psf_dec],fmt = '%s %s %.18e %.18e')
-
-
-#cat = str(sys.argv[2])
+out_cutout = np.c_[list_cutout,list_cutout_filter,list_cutout_ra,list_cutout_dec]
+out_psf = np.c_[list_psf,list_psf_filter,list_psf_ra,list_psf_dec]
+np.savetxt("/Volumes/LaCieSubaru/Gaia/James/cutouts_g.cat",out_cutout[out_cutout[:,1]=='G'],'%s \t %s \t %s \t %s')
+np.savetxt("/Volumes/LaCieSubaru/Gaia/James/cutouts_r.cat",out_cutout[out_cutout[:,1]=='R'],'%s \t %s \t %s \t %s')
+np.savetxt("/Volumes/LaCieSubaru/Gaia/James/cutouts_i.cat",out_cutout[out_cutout[:,1]=='I'],'%s \t %s \t %s \t %s')
+np.savetxt("/Volumes/LaCieSubaru/Gaia/James/cutouts_z.cat",out_cutout[out_cutout[:,1]=='Z'],'%s \t %s \t %s \t %s')
+np.savetxt("/Volumes/LaCieSubaru/Gaia/James/cutouts_y.cat",out_cutout[out_cutout[:,1]=='Y'],'%s \t %s \t %s \t %s')
+np.savetxt("/Volumes/LaCieSubaru/Gaia/James/psf_g.cat",out_psf[out_psf[:,1]=='G'],'%s \t %s \t %s \t %s')
+np.savetxt("/Volumes/LaCieSubaru/Gaia/James/psf_r.cat",out_psf[out_psf[:,1]=='R'],'%s \t %s \t %s \t %s')
+np.savetxt("/Volumes/LaCieSubaru/Gaia/James/psf_i.cat",out_psf[out_psf[:,1]=='I'],'%s \t %s \t %s \t %s')
+np.savetxt("/Volumes/LaCieSubaru/Gaia/James/psf_z.cat",out_psf[out_psf[:,1]=='Z'],'%s \t %s \t %s \t %s')
+np.savetxt("/Volumes/LaCieSubaru/Gaia/James/psf_y.cat",out_psf[out_psf[:,1]=='Y'],'%s \t %s \t %s \t %s')
+os.chdir("/Users/cerusu/GITHUB/zMstarPDF/python/image_utilities")
 #os.chdir(imgpath)
 #os.system("rm -f tmp.fits")
