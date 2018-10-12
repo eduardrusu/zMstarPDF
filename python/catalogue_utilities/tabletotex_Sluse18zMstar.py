58546ff2ba5bcb4b99d9a8256d96c03683554f2f
edi <cerusu@Cristians-MacBook-Pro.local>
2018-10-12 09:36:42 -1000

    edit

--- python/catalogue_utilities/tabletotex_Sluse18zMstar.py
+++ python/catalogue_utilities/tabletotex_Sluse18zMstar.py
@@ -41,16 +41,37 @@ data = np.c_[data,sep]
 sep = 12
 
 ################## impose conditions
+# for missing mass_med take its error bars to be typical for that mag range
+errbar18 = np.median(data[:,mass_med][(data[:,itot]>15) & (data[:,itot]<18) & (data[:,mass_med]!=-99)] - data[:,mass_inf][(data[:,itot]>15) & (data[:,itot]<18) & (data[:,mass_med]!=-99)])
+errbar19 = np.median(data[:,mass_med][(data[:,itot]>18) & (data[:,itot]<19) & (data[:,mass_med]!=-99)] - data[:,mass_inf][(data[:,itot]>18) & (data[:,itot]<19) & (data[:,mass_med]!=-99)])
+errbar20 = np.median(data[:,mass_med][(data[:,itot]>19) & (data[:,itot]<20) & (data[:,mass_med]!=-99)] - data[:,mass_inf][(data[:,itot]>19) & (data[:,itot]<20) & (data[:,mass_med]!=-99)])
+errbar21 = np.median(data[:,mass_med][(data[:,itot]>20) & (data[:,itot]<21) & (data[:,mass_med]!=-99)] - data[:,mass_inf][(data[:,itot]>20) & (data[:,itot]<21) & (data[:,mass_med]!=-99)])
+errbar22 = np.median(data[:,mass_med][(data[:,itot]>21) & (data[:,itot]<22) & (data[:,mass_med]!=-99)] - data[:,mass_inf][(data[:,itot]>21) & (data[:,itot]<22) & (data[:,mass_med]!=-99)])
+errbar23 = np.median(data[:,mass_med][(data[:,itot]>21) & (data[:,mass_med]!=-99)] - data[:,mass_inf][(data[:,itot]>21) & (data[:,mass_med]!=-99)])
+data[:,mass_best][data[:,mass_best] < 0] = 9
+data[:,mass_best][data[:,mass_med] > 0] = data[:,mass_med][data[:,mass_med] > 0]
+data[:,mass_inf][(data[:,mass_inf] < 0) & (data[:,itot]>15) & (data[:,itot]<18)] = data[:,mass_best][(data[:,mass_inf] < 0) & (data[:,itot]>15) & (data[:,itot]<18)] - errbar18
+data[:,mass_inf][(data[:,mass_inf] < 0) & (data[:,itot]>18) & (data[:,itot]<19)] = data[:,mass_best][(data[:,mass_inf] < 0) & (data[:,itot]>18) & (data[:,itot]<19)] - errbar19
+data[:,mass_inf][(data[:,mass_inf] < 0) & (data[:,itot]>19) & (data[:,itot]<20)] = data[:,mass_best][(data[:,mass_inf] < 0) & (data[:,itot]>19) & (data[:,itot]<20)] - errbar20
+data[:,mass_inf][(data[:,mass_inf] < 0) & (data[:,itot]>20) & (data[:,itot]<21)] = data[:,mass_best][(data[:,mass_inf] < 0) & (data[:,itot]>20) & (data[:,itot]<21)] - errbar21
+data[:,mass_inf][(data[:,mass_inf] < 0) & (data[:,itot]>21) & (data[:,itot]<22)] = data[:,mass_best][(data[:,mass_inf] < 0) & (data[:,itot]>21) & (data[:,itot]<22)] - errbar22
+data[:,mass_inf][(data[:,mass_inf] < 0) & (data[:,itot]>21)] = data[:,mass_best][(data[:,mass_inf] < 0) & (data[:,itot]>21)] - errbar23
+data[:,mass_sup][(data[:,mass_sup] < 0) & (data[:,itot]>15) & (data[:,itot]<18)] = data[:,mass_best][(data[:,mass_sup] < 0) & (data[:,itot]>15) & (data[:,itot]<18)] + errbar18
+data[:,mass_sup][(data[:,mass_sup] < 0) & (data[:,itot]>18) & (data[:,itot]<19)] = data[:,mass_best][(data[:,mass_sup] < 0) & (data[:,itot]>18) & (data[:,itot]<19)] + errbar19
+data[:,mass_sup][(data[:,mass_sup] < 0) & (data[:,itot]>19) & (data[:,itot]<20)] = data[:,mass_best][(data[:,mass_sup] < 0) & (data[:,itot]>19) & (data[:,itot]<20)] + errbar20
+data[:,mass_sup][(data[:,mass_sup] < 0) & (data[:,itot]>20) & (data[:,itot]<21)] = data[:,mass_best][(data[:,mass_sup] < 0) & (data[:,itot]>20) & (data[:,itot]<21)] + errbar21
+data[:,mass_sup][(data[:,mass_sup] < 0) & (data[:,itot]>21) & (data[:,itot]<22)] = data[:,mass_best][(data[:,mass_sup] < 0) & (data[:,itot]>21) & (data[:,itot]<22)] + errbar22
+data[:,mass_sup][(data[:,mass_sup] < 0) & (data[:,itot]>21)] = data[:,mass_best][(data[:,mass_sup] < 0) & (data[:,itot]>21)] + errbar23
 data = data[data[:,itot] <= 23]
 data = data[data[:,class_bpz] >= 0]
 data = data[data[:,sep] <= 120]
 data[:,z_eazy][data[:,spec] > 0] = data[:,spec][data[:,spec] > 0]
 data[:,z_inf][data[:,spec] > 0] = data[:,spec][data[:,spec] > 0]
 data[:,z_sup][data[:,spec] > 0] = data[:,spec][data[:,spec] > 0]
-data[:,mass_best][data[:,mass_med] > 0] = data[:,mass_med][data[:,mass_med] > 0]
-data[:,mass_inf][data[:,mass_inf] < 0] = data[:,mass_best][data[:,mass_inf] < 0] - 1
-data[:,mass_sup][data[:,mass_sup] < 0] = data[:,mass_best][data[:,mass_sup] < 0] + 1
+
 print np.shape(data)
+
+################## write tex file
 fileout = '/Users/cerusu/GITHUB/H0LiCOW/papers/WFI2033Environment/table_zMstar.tex'
 f = open(fileout,'w')
 for i in range(np.shape(data)[0]/2):
@@ -58,10 +79,3 @@ for i in range(np.shape(data)[0]/2):
 if np.shape(data)[0] % 2 == 1:
     f.write('%.5f & $%.5f$ & %.1f & $%.3f_{-%.3f}^{+%.3f}$ & $%.2f_{-%.2f}^{+%.2f}$ \\\\\n' % (data[:,ra][2*i+2],data[:,dec][2*i+2],data[:,sep][2*i+2],data[:,z_eazy][2*i+2],data[:,z_eazy][2*i+2]-data[:,z_inf][2*i+2],data[:,z_sup][2*i+2]-data[:,z_eazy][2*i+2],data[:,mass_best][2*i+2],data[:,mass_best][2*i+2]-data[:,mass_inf][2*i+2],data[:,mass_sup][2*i+2]-data[:,mass_best][2*i+2]))
 f.close()
-
-#np.savetxt(fileout,np.c_[data[:,ra],data[:,dec],data[:,itot],data[:,itot_err],data[:,u],data[:,u_err],data[:,g],data[:,g_err],data[:,r],data[:,r_err],data[:,i],data[:,i_err],data[:,z],data[:,z_err],data[:,Y],data[:,Y_err],data[:,J],data[:,J_err],data[:,H],data[:,H_err],data[:,K],data[:,K_err],data[:,ch1],data[:,ch1_err],data[:,ch2],data[:,ch2_err],data[:,ch3],data[:,ch3_err],data[:,ch4],data[:,ch4_err]],fmt='%.5f & $%.5f$ & $%.2f \pm %.2f$ & $%.2f \pm %.2f$ & $%.2f \pm %.2f$ & $%.2f \pm %.2f$ & $%.2f \pm %.2f$ & $%.2f \pm %.2f$ & $%.2f \pm %.2f$ & $%.2f \pm %.2f$ & $%.2f \pm %.2f$ & $%.2f \pm %.2f$ & $%.2f \pm %.2f$ & $%.2f \pm %.2f$ & $%.2f \pm %.2f$ & $%.2f \pm %.2f$ \\\\')
-#strin = '-99.00 \\\pm -99.00'
-#strout = '-'
-#os.system("sed -i -e \'s/%s/%s/g\' %s" % (strin,strout,fileout))
-
-#np.savetxt(masterfile[:-4] + "_WFI2033noIRACeazy_nobeta_testduplicate.cat",data.T,fmt='%s %s %s %s %.2f %.2f %d %d %.2f %.4f %.4f %.2f %.4f %.4f %.2f %.4f %.4f %.2f %.4f %.4f %.2f %.4f %.4f %.2f %.4f %.4f %.2f %.4f %.4f %.2f %.4f %.4f %.2f %.4f %.4f %.2f %.4f %.4f')
