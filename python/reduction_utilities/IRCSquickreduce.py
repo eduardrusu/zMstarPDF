fdef6b3f0781a8eceedf98fecd8b1d4fda66dfda
Cristian Eduard Rusu <cerusu@ucdavis.edu>
2019-02-19 14:34:09 -1000

    edit

--- python/reduction_utilities/IRCSquickreduce.py
+++ python/reduction_utilities/IRCSquickreduce.py
@@ -11,7 +11,7 @@ import glob
 os.system('/Applications/ds9.darwinsierra.7.5/ds9 &')
 path = str(sys.argv[1])
 #target = str(sys.argv[2])
-target = '1435'
+target = '1229'
 try: os.mkdir(path+target)
 except: pass
 os.chdir(path+target)
@@ -27,7 +27,7 @@ os.system('python /Users/cerusu/GITHUB/pyircs_imgred/frcheck.py %s' % target+'.c
 createflat = False
 if createflat == True:
     os.system('python /Users/cerusu/GITHUB/pyircs_imgred/imgred_all.py %s %s.fits --combine=median --skyflat --flat=%sflat.fits --bpm=/Users/cerusu/GITHUB/pyircs_imgred/DATA/ircs_bpmask.fits --start=0 --end=1' %(target+'.cat',target,target))
-    os.system('cp %sflat.fits ../' % target)
+    os.system('cp %sflat.fits ../../' % target)
 else:
-    os.system('python /Users/cerusu/GITHUB/pyircs_imgred/imgred_all.py %s %s.fits --combine=median --flat=../%sflat.fits --bpm=/Users/cerusu/GITHUB/pyircs_imgred/DATA/ircs_bpmask.fits --start=0 --end=8 --nsigma=10 --minpix=1000' %(target+'.cat',target, target))
+    os.system('python /Users/cerusu/GITHUB/pyircs_imgred/imgred_all.py %s %s.fits --combine=median --flat=../../flatFeb.fits --bpm=/Users/cerusu/GITHUB/pyircs_imgred/DATA/ircs_bpmask.fits --start=0 --end=8 --nsigma=10 --minpix=1000' %(target+'.cat',target))
 #  !!!!!!!!!!!!! When the pipeline says Select Object --> type a , Quit --> type q on the ds9 image I need to press first a than q on the same frame
